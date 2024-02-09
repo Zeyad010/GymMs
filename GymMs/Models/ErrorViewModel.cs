9b973e0903750e5bec2a9eeeefe08ed44cc79c15
Zeyad01 <zeyad.elmahdy@gmail.com>
2024-02-09 22:23:16 +0200

    Add project files.

---
+++ GymMs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GymMs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
