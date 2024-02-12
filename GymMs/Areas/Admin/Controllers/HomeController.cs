100d740084e47572e160d17f433aa61d5aeedb5d
Zeyad01 <zeyad.elmahdy@gmail.com>
2024-02-12 09:20:29 +0200

    Set Admin Panel

---
+++ GymMs/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using GymMs.Data;
+using GymMs.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymMs.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public HomeController(ApplicationDbContext db)
+        {
+                _db = db;
+        }
+        public IActionResult List()
+        {
+            List<UserM> objUsersList =_db.TbUsers.ToList();
+
+            return View(objUsersList);
+        }
+    }
+}
